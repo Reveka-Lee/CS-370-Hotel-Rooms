4b2d0c98d8ff93ebd00cb01a3667371767ef6343
Reveka-Lee <55007255+Reveka-Lee@users.noreply.github.com>
2020-04-24 14:14:11 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+var index = 0;
+next();
+
+function next() {
+  var i;
+  var x = document.getElementsByClassName("pic");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  index++;
+  if (index > x.length) {
+    index = 1;
+  }    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 9000);    
+}
